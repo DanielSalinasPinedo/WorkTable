4edbbfd562868ff233f7a93b517669dbe7598e77
DANIEL ALEJANDRO SALINAS PINEDO <SFDaniefranco@gmail.com>
2026-02-18 10:04:14 -0500

    Delete front/back directory

--- front/back/src/routes/SIOS.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getSIOSPte } from "../controllers/SIOS.controller";
-
-const router = Router()
-
-router.get('/SIOS-Pte/:cedula', getSIOSPte)
-
-export default router
